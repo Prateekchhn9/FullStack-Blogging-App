44dc3700b0429cf7d4883438e04812149a12d5a4
Prateek Chauhan <60933063+Prateekchhn9@users.noreply.github.com>
2025-07-31 13:39:24 +0200

    Update output.tf
    devopsshack-------prtk

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.prtk.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.prtk.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.prtk_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value =  aws_subnet.prtk_subnet[*].id
 }
